7c35bc12d823b952d5270c70720690b4763d5c67
daneshm <daneshm@student.chalmers.se>
2022-11-15 21:22:52 +0100

    added login and sign in pages

--- dentistimo_ui/src/components/Header/HeaderElements.ts
+++ dentistimo_ui/src/components/Header/HeaderElements.ts
@@ -11,6 +11,7 @@ export const NavbarContainer = styled.div`
     display: flex;
     justify-content: space-between;
     height: 80px;
+    margin-right: 3rem;
     width: 100%;
     padding: 0 24px;
     max-width: 1100px;
