0a1878e3f8c1c1668a1d0e990b1c73f7899b2c61
Aditya Khadkikar <guskhadad@student.gu.se>
2022-11-30 17:16:26 +0100

    improved autocomplete to dynamically render according to dentistries.json
    Co-authored-by: antongol <antongol@student.chalmers.se>

--- dentistimo_ui/src/components/SearchBar/SearchBar.tsx
+++ dentistimo_ui/src/components/SearchBar/SearchBar.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { useState } from 'react';
 import './SearchBar.css';
+import dentistries from '../../data/dentistries';
 import { Stack, Autocomplete, TextField } from '@mui/material';
 import { borders } from '@mui/system';
 
-const dentistries = ['Tooth Fairy Dentist', 'Your Dentist', 'The Crown', 'Liseberg Dentists']
-
-export const SearchBar = () => {
+const SearchBar = () => {
     const [value, setValue] = useState<string | null> (null) 
+    const dentistriesList: string[] = dentistries.map((dentistry, index) => {return dentistry.name});
+    
     return (
         <Stack spacing= {2}>
             <Autocomplete
             disablePortal
             id="combo-box-demo"
             sx={{ width: 300 , borderRadius: '15px' }}
-            options={dentistries}
+            options={dentistriesList}
             renderInput={(params) => <TextField {...params} label='Search by name or location ...'/>}
             value={value}
             onChange={(event: any, newValue: string | null) => setValue(newValue)}
@@ -23,3 +24,5 @@ export const SearchBar = () => {
         </Stack>
     )
 }
+
+export default SearchBar;
